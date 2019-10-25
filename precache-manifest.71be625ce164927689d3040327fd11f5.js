e70ba88779b67433ccf95b08c2bd7a75ff500d24
Tuvalnik12 <milecki46@gmail.com>
2019-10-25 11:58:41 +0300

    Updates

---
+++ precache-manifest.71be625ce164927689d3040327fd11f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a32dc2405abeb79a0aa3790df12c925",
+    "url": "/FutSTAT/index.html"
+  },
+  {
+    "revision": "089e983723233086610d",
+    "url": "/FutSTAT/static/css/main.5a767b2b.chunk.css"
+  },
+  {
+    "revision": "94acf295ed532edabf50",
+    "url": "/FutSTAT/static/js/2.233d61d9.chunk.js"
+  },
+  {
+    "revision": "089e983723233086610d",
+    "url": "/FutSTAT/static/js/main.00c60491.chunk.js"
+  },
+  {
+    "revision": "22337635c1394b0d161e",
+    "url": "/FutSTAT/static/js/runtime-main.fcccfe8c.js"
+  }
+]);
